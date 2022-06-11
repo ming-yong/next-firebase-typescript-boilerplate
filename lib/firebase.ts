0e1e6e8f75d956f6e7cf6f1a7de02d843c88b7f3
Ming Yong <mingyong.dev@gmail.com>
2022-06-10 23:48:26 -0400

    update readme

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+    // your config SDK here
+};
+ 
+//Avoid NEXT from running initialization twice
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
